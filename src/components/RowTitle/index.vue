9b6acf073ddafff7f8695007bb70abf9ee8ffcec
qunhua <769320266@qq.com>
2020-03-20 11:26:28 +0800

    发布组件到npm

---
+++ src/components/RowTitle/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row-title">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RowTitle',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.row-title{
+  font-size: 18px;
+  color: #343434;
+  background: #f5f5f5;
+  border-left: 4px solid #00bff0;
+  padding: 8px 0 8px 8px;
+  text-align: left;
+}
+</style>
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import RowTitle from './RowTitle'
+
+RowTitle.install = Vue => {
+  Vue.component(RowTitle.name, RowTitle)
+}
+
+export default RowTitle
